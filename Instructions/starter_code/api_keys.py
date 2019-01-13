7d2ed38bcfa727c2765019ef8e7c750bce4bfa14
Lori H <lorieharris@gmail.com>
2019-01-13 12:14:18 -0500

    removed my api key 'DOH!

--- Instructions/starter_code/api_keys.py
+++
@@ -1,2 +0,0 @@
-# OpenWeatherMap API Key
-api_key = "913877f6dc8a4d2b224b59aa078371fa"
--- api_keys.py
+++ api_keys.py
@@ -1,2 +1,2 @@
 # OpenWeatherMap API Key
-api_key = "913877f6dc8a4d2b224b59aa078371fa"
+api_key = "your api key here"
